214fe9e37ab5115c5a8e10884b7bdfb84a5d515f
wonjinism <wonjinism@gmail.com>
2019-01-22 11:07:04 +0900

    Move a Kanji-Splitter project
    Remove a sub project, Kanji Splitter
    Create a new project for Kanji Splitter

--- KanjiSplitter/src/main/java/io/zerone/kanjisplitter/DAO/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package io.zerone.kanjisplitter.DAO;
-
-import io.zerone.kanjisplitter.VO.User;
-
-public interface UserMapper {
-	public int insertUser(User user);
-	public User selectUser(String user_id);
-}
